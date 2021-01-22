fd28e9b8b5af3fac85c266f623ba521b2f61dbec
Lugardo Rodrigo <lugardorodrigo@gmail.com>
2021-01-22 14:10:43 -0600

    comiit inicial

---
+++ bin/ejemplodeclase22.dart
@@ -0,0 +1,19 @@
+import 'package:ejemplodeclase22/ejemplodeclase22.dart' as ejemplodeclase22;
+import 'persona.dart';
+import 'triangulo.dart';
+
+void main(List<String> arguments) {
+  /*
+  Triangulo triangulo = new Triangulo();
+
+  triangulo.base = 7.5;
+  triangulo.altura = 23.5;
+
+  print('El area del triangulo es: ${triangulo.area}');
+  */
+
+  Persona p = new Persona();
+  p.nombre = 'Rodrigo';
+
+  print('El nombre es: ${p.nombrePersona}');
+}
---
+++ test/ejemplodeclase22_test.dart
@@ -0,0 +1,8 @@
+import 'package:ejemplodeclase22/ejemplodeclase22.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
